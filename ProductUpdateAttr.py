8d43819b666c775766fe727bd7c6aac51b82348a
Siggib <siggib007@users.noreply.github.com>
2026-05-19 11:10:13 +0000

    added content len to audit

--- ProductUpdateAttr.py
+++ ProductUpdateAttr.py
@@ -1593,7 +1593,7 @@ def main():
     if objFileOut is None or isinstance(objFileOut, str):
       objFileOut = None
       LogEntry("Unable to open output file {}, error: {}".format(strOutFileName, objFileOut),0,True)
-    objFileOut.write("Brand,SKU,Name,Type,Status,Descr len,Existing Attribute Count,Local Attribute Count,Description Attributes Count\n")
+    objFileOut.write("Brand,SKU,Name,Type,Status,Descr len,Content len,Existing Attribute Count,Local Attribute Count,Description Attributes Count\n")
 
   # Here is basic prep work for all actions
   iPage = 1
@@ -1753,8 +1753,9 @@ def main():
 
       if strAction == "AUDIT":
         # write out the audit file
-        objFileOut.write("{},{},{},{},{},{},{},{},{}\n".format(strBrand.strip(), dictProduct["sku"],
-            dictProduct["name"].replace(","," ").strip(), dictProduct["type"], dictProduct["status"], len(dictProduct["description"]), len(lstProdAttribs), iLocalCount, len(dictAttributes)))
+        objFileOut.write("{},{},{},{},{},{},{},{},{},{}\n".format(strBrand.strip(), dictProduct["sku"],
+            dictProduct["name"].replace(","," ").strip(), dictProduct["type"], dictProduct["status"], len(dictProduct["description"]),
+            len(StripHTML(dictProduct["description"])), len(lstProdAttribs), iLocalCount, len(dictAttributes)))
         objFileOut.flush()
 
   if strAction == "MIKROTIK" and strMikrotikToken and strMikroTikURL and lstReport:
